0ebb9cf03e416f5dec302664f03f29c35e632937
鈴木浩章 <hroaki.suzuki@gmail.com>
2021-01-08 19:43:07 +0900

    css適用

--- src/myblock.js
+++ src/myblock.js
@@ -2,6 +2,8 @@ import MyBlockEditing from './myblock/myblockediting';
 import MyBlockUI from './myblock/myblockui';
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 
+import '../theme/myblockstyle.css';
+
 export default class MyBlock extends Plugin {
     static get requires() {
         return [ MyBlockEditing, MyBlockUI ];
